860880069a2a77521c8694538af36818af58f0d7
asunuk <146172407+asunuk@users.noreply.github.com>
2024-09-20 15:04:18 +0300

    Test working in Kvantorium

--- Assets/Scripts/API/Network/NetworkFpcRole.cs
+++ Assets/Scripts/API/Network/NetworkFpcRole.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Scripts.API.Roles.Network
 {
-	internal class NetworkFpcRole
+public class NetworkFpcRole
 	{
+		public NetworkManager netManager;
 	}
 }
